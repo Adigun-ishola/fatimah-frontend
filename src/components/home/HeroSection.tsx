813530b8ea24b034d1ec204e7ddeff71f92c3227
adigun ishola <adigunishola001@gmail.com>
2026-09-23 13:57:10 +0100

    chnages

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -20,7 +20,7 @@ export default function HeroSection() {
       <div className="absolute top-20 right-10 w-72 h-72 bg-[#c9a84c]/10 rounded-full blur-3xl" />
       <div className="absolute bottom-20 left-10 w-96 h-96 bg-[#c9a84c]/5 rounded-full blur-3xl" />
 
-      <div className="container-custom relative z-10 pt-30">
+      <div className="container-custom relative z-10 sm:pt-30 sm:pb-30">
         <div className="grid lg:grid-cols-2 gap-12 items-center">
           {/* Text Content */}
           <motion.div
